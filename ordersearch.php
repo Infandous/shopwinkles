e033a4380d43ee3d28cf23f10469d2b1358453cb
jsingh2412 <60803017+jsingh2412@users.noreply.github.com>
2024-04-16 01:35:38 -0500

    fixed database query problems

--- ordersearch.php
+++ ordersearch.php
@@ -1,4 +1,7 @@
 <?php
+//ini_set('display_errors', 1);
+//ini_set('display_startup_errors', 1);
+//error_reporting(E_ALL);
 // connect to server
 $conn = mysqli_connect('mysql.eecs.ku.edu', '447s24_j507s861', 'tieTexo3', '447s24_j507s861');
 if (!$conn) {
@@ -9,7 +12,7 @@ if (!$conn) {
 $search = mysqli_real_escape_string($conn, $_GET['search']);
 
 // set and send query
-$query = "SELECT * FROM Order WHERE OrderID LIKE '$search%'";
+$query = "SELECT * FROM OrderInfo WHERE OrderID = '$search'";
 $result = mysqli_query($conn, $query);
 
 // Check for errors
@@ -18,17 +21,16 @@ if (!$result) {
 }
 
 echo '<div class="order-container">';
-    $row = mysqli_fetch_assoc($result);
-    echo '<div class="order-box">';
-    echo '<p>' .$row['Order_id'] . '</p>';
-    echo '<p> Product Id: ' .$row['Product_id'] . '</p>';
-    echo '<p> Customer Id: ' .$row['Customer_id'] . '</p>';
-    echo '<p> Date Purchased: ' .$row['purchase_date'] . '</p>';
-    echo '<p> Delivery_date' .$row['delivery_date'] . '</p>';
-    echo '</div>';
-
-
+$row = mysqli_fetch_assoc($result);
+echo '<div class="order-box">';
+echo '<p>' . $row['OrderID'] . '</p>';
+//echo '<p> Product Id: ' . $row['ProductID'] . '</p>';
+//echo '<p> Customer Id: ' . $row['CustomerID'] . '</p>';
+echo '<p> Date Purchased: ' . $row['Date_Purchased'] . '</p>';
+echo '<p> Delivery Date:' . $row['Expected_Delivery'] . '</p>';
 echo '</div>';
+echo '</div>';
+
 $_GET['search'] = '';
 
 mysqli_free_result($result);
