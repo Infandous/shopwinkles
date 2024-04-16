0c913c6e034d4776af1334f5563f2f0d407a3338
jsingh2412 <60803017+jsingh2412@users.noreply.github.com>
2024-04-16 01:17:54 -0500

    Update signin_process.php

--- signin_process.php
+++ signin_process.php
@@ -1,7 +1,6 @@
 <?php
 // Start or resume the session
 session_start();
-
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // connect to server
     $conn = mysqli_connect('mysql.eecs.ku.edu', '447s24_j507s861', 'tieTexo3', '447s24_j507s861');
@@ -9,28 +8,29 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         die('Could not connect: ' . mysqli_connect_error());
     }
 
-    // Retrieve form data
+    // get form data
     $email = mysqli_real_escape_string($conn, $_POST['email']);
     $password = mysqli_real_escape_string($conn, $_POST['password']);
 
-    // Prepare SQL query
+    // write query
     $sql = "SELECT * FROM Customer WHERE Email = '$email' AND Password = '$password'";
     $result = mysqli_query($conn, $sql);
 
     if (mysqli_num_rows($result) == 1) {
-        // Sign in successful
+        // successful signin
         $row = mysqli_fetch_assoc($result);
-        // Store user data in session variables
+        // make session variables
         $_SESSION['customerID'] = $row['CustomerID'];
         $_SESSION['name'] = $row['Name'];
 
-        // Redirect to dashboard or home page
+        // redirect user
         header("Location: home.html");
         exit();
     } else {
-        // Sign in failed
+        // sign in failed
+        //this error is not used write yet.
         $_SESSION['error'] = "Invalid email or password. Please try again.";
-        header("Location: signin.html"); // Redirect back to sign-in page
+        header("Location: signin.html"); 
         exit();
     }
 
