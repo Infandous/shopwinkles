3f2750abaed5a1ef19b193785e19dec6d289b059
jsingh2412 <60803017+jsingh2412@users.noreply.github.com>
2024-04-17 11:26:34 -0500

    displays name along with everything else!

--- ordersearch.php
+++ ordersearch.php
@@ -1,7 +1,6 @@
 <?php
-//ini_set('display_errors', 1);
-//ini_set('display_startup_errors', 1);
 //error_reporting(E_ALL);
+//ini_set('display_errors', 1);
 // connect to server
 $conn = mysqli_connect('mysql.eecs.ku.edu', '447s24_j507s861', 'tieTexo3', '447s24_j507s861');
 if (!$conn) {
@@ -12,7 +11,10 @@ if (!$conn) {
 $search = mysqli_real_escape_string($conn, $_GET['search']);
 
 // set and send query
-$query = "SELECT * FROM OrderInfo WHERE OrderID = '$search'";
+$query = "SELECT OrderInfo.OrderID, OrderInfo.Date_Purchased, OrderInfo.Expected_Delivery, Customer.Name 
+            FROM OrderInfo
+            JOIN Customer ON OrderInfo.CustomerID = Customer.CustomerID 
+            WHERE OrderInfo.OrderID = '$search'";
 $result = mysqli_query($conn, $query);
 
 // Check for errors
@@ -20,19 +22,19 @@ if (!$result) {
     die('Query failed: ' . mysqli_error($conn));
 }
 
-echo '<div class="order-container">';
-$row = mysqli_fetch_assoc($result);
-echo '<div class="order-box">';
-echo '<p>' . $row['OrderID'] . '</p>';
-//echo '<p> Product Id: ' . $row['ProductID'] . '</p>';
-//echo '<p> Customer Id: ' . $row['CustomerID'] . '</p>';
-echo '<p> Date Purchased: ' . $row['Date_Purchased'] . '</p>';
-echo '<p> Delivery Date:' . $row['Expected_Delivery'] . '</p>';
-echo '</div>';
-echo '</div>';
+if(mysqli_num_rows($result)>0){
+    echo '<div class="order-container">';
+    $row = mysqli_fetch_assoc($result);
+    echo '<div class="order-box">';
+    echo '<p>' . $row['OrderID'] . '</p>';
+    echo '<p> Customer Name: ' . $row['Name'] . '</p>';
+    echo '<p> Date Purchased: ' . $row['Date_Purchased'] . '</p>';
+    echo '<p> Delivery Date:' . $row['Expected_Delivery'] . '</p>';
+    echo '</div>';
+    echo '</div>';
 
 $_GET['search'] = '';
-
+}
 mysqli_free_result($result);
 mysqli_close($conn);
 ?>
